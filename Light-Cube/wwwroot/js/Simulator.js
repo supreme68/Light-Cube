a3d6f711d23c9ce3966cea1aef445a80b73e2da0
Димитър <dimitararnaudov68@gmail.com>
2018-02-28 13:13:42 +0200

    MADE CRUCIAL CHANGES

--- Light-Cube/wwwroot/js/Simulator.js
+++ Light-Cube/wwwroot/js/Simulator.js
@@ -11,6 +11,7 @@ function preload() {
 
 
 function create() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     //Changes background
     game.stage.backgroundColor = '#87FF65';
     //Enables Arcade Physics
@@ -24,34 +25,60 @@ function update() {
  
 }
 
+
 function Resistor() {
-    let sprite = game.add.sprite(0, 0, 'skeleton');
-    sprite.inputEnabled = true;
-    sprite.input.enableDrag();
-    sprite.events.onDragStart.add(startDrag, this);
-    sprite.events.onDragStop.add(stopDrag, this);
+    this.resistor = game.add.sprite(0, 0, 'skeleton');
+    this.resistance  = 10;
+    this.voltage = 15;
+    this.current = 20;
+    this.description = "resistors are dope";
+    resistor.inputEnabled = true;
+    resistor.events.onInputDown.add(Show, this);
+    resistor.input.enableDrag();
+    resistor.events.onDragStart.add(startDrag, this);
+    resistor.events.onDragStop.add(stopDrag, this);
 }
 
 function Led() {
-    let sprite = game.add.sprite(0, 0, 'skeleton');
-    sprite.inputEnabled = true;
-    sprite.input.enableDrag();
-    sprite.events.onDragStart.add(startDrag, this);
-    sprite.events.onDragStop.add(stopDrag, this);
+    this.led = game.add.sprite(0, 0, 'skeleton');
+    this.resistance = 30;
+    this.voltage = 30;
+    this.current = 10;
+    this.description = "LEDs are dope ";
+    led.inputEnabled = true;
+    led.events.onInputDown.add(Show, this);
+    led.input.enableDrag();
+    led.events.onDragStart.add(startDrag, this);
+    led.events.onDragStop.add(stopDrag, this);
 }
 
 function Battery() {
-    let sprite = game.add.sprite(0, 0, 'skeleton');
-    sprite.inputEnabled = true;
-    sprite.input.enableDrag();
-    sprite.events.onDragStart.add(startDrag, this);
-    sprite.events.onDragStop.add(stopDrag, this);
+    this.battery = game.add.sprite(0, 0, 'skeleton');
+    this.resistance = 12;
+    this.voltage = 40;
+    this.current = 70;
+    battery.inputEnabled = true;
+    battery.events.onInputDown.add(Show, this);
+    battery.input.enableDrag();
+    battery.events.onDragStart.add(startDrag, this);
+    battery.events.onDragStop.add(stopDrag, this    );    
 }
 
 function Conductor() {
-    let sprite = game.add.sprite(0, 0, 'skeleton');
-    sprite.inputEnabled = true;
-    sprite.input.enableDrag();
-    sprite.events.onDragStart.add(startDrag, this);
-    sprite.events.onDragStop.add(stopDrag, this);
+    this.conductor = game.add.sprite(0, 0, 'skeleton');
+    this.resistance = 25;
+    this.voltage = 12;
+    this.current = 13;
+    conductor.inputEnabled = true;
+    conductor.input.enableDrag();
+    conductor.events.onInputDown.add(Show, this);
+    conductor.events.onDragStart.add(startDrag, this);
+    conductor.events.onDragStop.add(stopDrag, this);
+}
+
+
+function Show(){
+    document.getElementById("resistance").innerHTML = this.resistance;
+    document.getElementById("voltage").innerHTML = this.voltage;
+    document.getElementById("current").innerHTML = this.    current;       
 }
