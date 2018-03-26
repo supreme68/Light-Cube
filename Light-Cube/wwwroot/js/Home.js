3f5fbcf3c08c4477e070fb7b816993ba438b4bbf
dimitar aranaudov <dimitararnaudov68@gmail.com>
2018-03-26 12:30:02 +0300

    Initial

--- Light-Cube/wwwroot/js/Home.js
+++
@@ -1,7 +0,0 @@
-﻿//This is Used for the Back To Top Button
-$(document).ready(function () {
-$('#toTop').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 800);
-    return false;
-});
-});
